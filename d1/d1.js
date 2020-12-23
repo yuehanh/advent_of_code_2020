ead707052cbd94e63da26740e26352d5b22e93d3
Yuehan <yuehan.huang@protonmail.com>
2020-12-23 14:19:31 -0800

    Complete part 2

--- d1/d1.js
+++ d1/d1.js
@@ -5,7 +5,7 @@ const parseInput = async () => {
   return data.split("\n");
 };
 const sumTo2020 = async () => {
-  const nums = await (await parseInput()).map(Number); //same as .map((num) => Number(num));
+  const nums = (await parseInput()).map(Number); //same as .map((num) => Number(num));
   const set = new Set();
   for (const num of nums) {
     const diff = 2020 - num;
@@ -17,3 +17,23 @@ const sumTo2020 = async () => {
 };
 
 sumTo2020().then(console.log);
+
+const threeSumTo2020 = async () => {
+  const nums = (await parseInput()).map(Number); //same as .map((num) => Number(num));
+  for (let i = 0; i < nums.length; i++) {
+    const set = new Set();
+    const twoSum = 2020 - nums[i];
+    for (let j = i + 1; j < nums.length; j++) {
+      const diff = twoSum - nums[j];
+      if (set.has(diff)) {
+        console.log(diff);
+        console.log(nums[i]);
+        console.log(nums[j]);
+        return nums[i] * nums[j] * diff;
+      }
+      set.add(nums[j]);
+    }
+  }
+};
+
+threeSumTo2020().then(console.log);
